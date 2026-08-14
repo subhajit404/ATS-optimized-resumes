2b1bece6ff2a6e9c2064c1b0dd3be21fa11440ca
subhajit404 <subhajitpatra285@gmail.com>
2026-08-14 17:44:58 +0530

    Update auth controller and auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,106 +1,21 @@
-const userModel = require("../models/user.model")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const { Router } = require('express')
+const authController = require("../controllers/auth.controller")
+
+const authRouter = Router()
 
 /**
- * @name registerUserController
- * @description register a new user, expects username, email and password in the request body
+ * @route POST /api/auth/register
+ * @description Register a new user
  * @access Public
  */
-async function registerUserController(req, res) {
-
-    const { username, email, password } = req.body
-
-    if (!username || !email || !password) {
-        return res.status(400).json({
-            message: "Please provide username, email and password"
-        })
-    }
-
-    const isUserAlreadyExists = await userModel.findOne({
-        $or: [ { username }, { email } ]
-    })
-
-    if (isUserAlreadyExists) {
-        return res.status(400).json({
-            message: "Account already exists with this email address or username"
-        })
-    }
-
-    const hash = await bcrypt.hash(password, 10)
-
-    const user = await userModel.create({
-        username,
-        email,
-        password: hash
-    })
-
-    const token = jwt.sign(
-        { id: user._id, username: user.username },
-        process.env.JWT_SECRET,
-        { expiresIn: "1d" }
-    )
-
-    res.cookie("token", token)
-
-
-    res.status(201).json({
-        message: "User registered successfully",
-        user: {
-            id: user._id,
-            username: user.username,
-            email: user.email
-        }
-    })
-
-}
+authRouter.post("/register", authController.registerUserController)
 
 
 /**
- * @name loginUserController
- * @description login a user, expects email and password in the request body
+ * @route POST /api/auth/login
+ * @description login user with email and password
  * @access Public
  */
-async function loginUserController(req, res) {
+authRouter.post("/api/login", authController.loginUserController)
 
-    const { email, password } = req.body
-
-    const user = await userModel.findOne({ email })
-
-    if (!user) {
-        return res.status(400).json({
-            message: "Invalid email or password"
-        })
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password)
-
-    if (!isPasswordValid) {
-        return res.status(400).json({
-            message: "Invalid email or password"
-        })
-    }
-
-    const token = jwt.sign(
-        { id: user._id, username: user.username },
-        process.env.JWT_SECRET,
-        { expiresIn: "1d" }
-    )
-
-    res.cookie("token", token)
-    res.status(200).json({
-        message: "User loggedIn successfully.",
-        user: {
-            id: user._id,
-            username: user.username,
-            email: user.email
-        }
-    })
-}
-
-
-
-module.exports = {
-    registerUserController,
-    loginUserController
-}
+module.exports = authRouter
