5bcb44bec0f63f29a56d0700ea0b831f7509f96a
subhajit404 <subhajitpatra285@gmail.com>
2026-08-14 15:21:06 +0530

    add Auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+const userModel = require("../models/user.model")
+
+
+/**
+ * @name registerUserController
+ * @description register a new user, expects username, email and password in the request body
+ * @access Public
+ */
+
+async function registerUserController(req, res) {
+
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide username, email and password"
+        })
+    }
+
+      const isUserAlreadyExists = await userModel.findOne({
+        $or: [ { username }, { email } ]
+    })
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "Account already exists with this email address or username"
+        })
+    }
+}
